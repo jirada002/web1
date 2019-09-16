3ac3d664ee71af50342721096e36c400edde85fc
jirada002 <jiradanice-st@rmutsb.ac.th>
2019-09-16 19:45:24 +0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using jirada.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace jirada.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult plus(int id)
+        {
+            int i = id + 10;
+            ViewData["sum"] = i;
+            return View();
+        }
+        public IActionResult multiply(int id)
+        {
+            int m = id * 10;
+            ViewData["resualt"] = m;
+            return View();
+        }
+        [Route("minus/{a}/{b}")]
+        public IActionResult minus(int a, int b)
+        {
+            int mi = a - b;
+            ViewData["pollub"] = mi;
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ContactPost(IFormCollection values)
+        {
+            ViewData["Name"] = values["name"];
+            ViewData["Email"] = values["email"];
+            ViewData["ShowAlert"] = true;
+            return View("Index");
+        }
+        public IActionResult Contact2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactPost2(Contact model)
+        {
+            ViewData["Name"] = model.Name;
+            ViewData["Email"] = model.Email;
+            ViewData["ShowAlert"] = true;
+            return View("Index");
+        }
+
+    }
+}
